0c894164fe986a50ceaa7be02d226514d75650d6
SREENIVASAN-PRASANTH <prasanthshot@gmail.com>
2024-07-25 09:49:48 +0530

    added postcss

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className="text-red-500">Hello</h1>
     </div>
-  )
+  );
 }
